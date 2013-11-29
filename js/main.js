7ab43b3cd000394604476a471ac42ff2f50a649a
Andi Staub <sta@panter.ch>
2013-11-29 11:17:28 +0100

    Finalized page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $('a.menu-link').click(function() {
+    $('#menu').toggleClass('active');
+    return false;
+  });
+
+  $('.navbar li a').click(function() {
+    $('#menu').removeClass('active');
+  });
+
+  $('.navbar a.go-to-top').click(function() {
+    $('#menu').removeClass('active');
+  });
+});
