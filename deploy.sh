a74c6a1cd117818fa2a2bb693f05ec6aa90c241d
Andi Staub <sta@panter.ch>
2014-09-02 09:11:26 +0200

    deploy script deploys to live.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 chmod -R +r build/
-rsync -av build/ panterch@www.panter.ch:~/beta.panter.ch/
-ssh panterch@www.panter.ch 'chmod -R +r ~/beta.panter.ch'
+
+#rsync -av --delete build/ panterch@www.panter.ch:~/beta.panter.ch/
+#ssh panterch@www.panter.ch 'chmod -R +r ~/beta.panter.ch'
+
+rsync -av --delete build/ panterch@www.panter.ch:~/www.panter.ch/
+ssh panterch@www.panter.ch 'chmod -R +r ~/www.panter.ch'
