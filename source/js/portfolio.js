f9de625138127797934398f10227337738a4b63f
Alexis Reigel <mail@koffeinfrei.org>
2016-02-02 18:37:32 +0100

    use skrollr for better parallax
    in chrome the prev. parallax version resulted in a very unsmooth
    scrolling experience.

--- source/js/portfolio.js
+++ source/js/portfolio.js
@@ -1,5 +1,6 @@
 /*
  *= require './vendor/jquery-1.11.1.min'
+ *= require './vendor/skrollr.js'
  *= require_self
  *= require_tree ./portfolio
  */
--- source/js/portfolio/parallax.js
+++ source/js/portfolio/parallax.js
@@ -1,23 +1,3 @@
 $(document).ready(function() {
-  parallax();
+  skrollr.init();
 });
-
-$(window).scroll(function() {
-  parallax();
-});
-
-function parallax(){
-  $(".js-parallax-window").each(function() {
-    var plxWindow = $(this);
-    var plxBackground = plxWindow.find(".js-parallax-background");
-
-    var plxWindowTopToPageTop = $(plxWindow).offset().top;
-    var windowTopToPageTop = $(window).scrollTop();
-    var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
-
-    var plxSpeed = 0.65;
-
-    plxBackground.css('top', - (plxWindowTopToWindowTop * plxSpeed) + 'px');
-    // plxBackground.animate({top: - (plxWindowTopToWindowTop * plxSpeed) + 'px'}, 0.001);
-  });
-}
